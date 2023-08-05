7a0d725aeacd63f690a78a293cf312e4e18011fd
mrredo <74524695+mrredo@users.noreply.github.com>
2023-08-05 20:00:02 +0300

    Create bumbas.go

---
+++ bumbas.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+func maxLayers(blocks int) int {
+	return int(math.Sqrt(float64(blocks)))
+}
+func ballsUsed(layers int) int {
+	return (layers * (1 + (2*layers - 1))) / 2
+}
+func main() {
+
+	blocktests := []int{
+		1,
+
+	}
+	for _, v := range blocktests {
+		l := maxLayers(v)
+		tb := ballsUsed(l)
+		bl := v - tb
+		fmt.Println(LastPosOfBall(l, bl).String())
+	}
+
+}
+type Pos struct {
+	y int
+	x int
+
+}
+func (p Pos) String() string {
+	return fmt.Sprintf("%d %d", p.x, p.y)
+}
+func LastPosOfBall(layers int, ballsleft int) Pos {
+	if ballsleft == 0 {
+		return Pos{
+			x: 0,
+			y: layers,
+
+		}
+	}
+	if layers == ballsleft {
+		p := Pos{
+			x: layers-ballsleft+1,
+		}
+		p.y = layers - p.x+1
+		return p
+	} else if  layers > ballsleft {
+		p := Pos{
+			x: layers-ballsleft+1,
+		}
+		p.y = layers - p.x + 1
+		return p
+	} else if ballsleft > layers {
+		p := Pos{
+			x: -2 * layers+ballsleft-1,
+		}
+		p.y = layers - (p.x * -1) + 1
+		return p
+	}
+	return Pos{}
+}
